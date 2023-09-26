24935a7fcc660c7e1c9a097a409822e187ed7f37
kp1542a <122700455+kp1542a@users.noreply.github.com>
2023-09-25 22:16:53 -0400

    Create Main.py

---
+++ src/Main.py
@@ -0,0 +1,15 @@
+import curses
+from curses import wrapper
+from Maze import maze
+from BreadthFirstSearch import find_path
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    find_path(maze, stdscr)
+    stdscr.getch()
+
+
+wrapper(main)
